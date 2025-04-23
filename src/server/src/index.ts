2758d7f5d1a7f35bc31a4741838c6a0553534a7f
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-23 19:03:25 -0400

    feat(server): add chat endpoint with streaming response and error handling

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { generateContent } from "./gemini";
+import { streamText } from "hono/streaming";
+import * as console from "node:console";
+import { HTTPException } from "hono/http-exception";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.post("/chat", async (c) => {
+  const input = await c.req.text();
+  console.log(input);
+  const response = await generateContent(input);
+  return streamText(
+    c,
+    async (stream) => {
+      for await (const chunk of response) {
+        await stream.write(chunk.text!);
+      }
+      stream.writeln(Bun.env.NODE_ENV as string);
+    },
+    async (error, stream) => {
+      await stream.write(error.message);
+    },
+  );
+});
+app.onError((error, c) => {
+  if (error instanceof HTTPException) {
+    // Get the custom response
+    return error.getResponse();
+  }
+  return c.text(error.message);
+});
 
-export default app
+export default app;
