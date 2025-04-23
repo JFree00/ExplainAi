ecea9cc09c3174aee3dd582e906c5ffe21821b7d
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-23 19:03:36 -0400

    feat(server): add fallback model and improve prompt handling

--- src/server/src/gemini/index.ts
+++ src/server/src/gemini/index.ts
@@ -3,18 +3,24 @@ import { GoogleGenAI } from "@google/genai";
 const gemini = new GoogleGenAI({ apiKey: Bun.env.GEMINI_API_KEY });
 export const geminiConfig = {
   model: "gemini-2.5-flash-preview-04-17",
+  fallback: "gemini-2.0-flash", // also used for development
   prompt:
     "Analyze the provided content (which could be text or an image) and explain its main subject, meaning, or purpose" +
     " in a clear and concise way.",
   systemPrompt:
     "You are a helpful AI assistant. Your purpose is to clearly and concisely explain text or images " +
-    "provided by the user. Explain only the provided content. Do not speculate, add external information, or give " +
-    "opinions. Stick strictly to describing or explaining what is present in the text or image. Do not offer any other" +
-    " assistance",
+    "provided by the user. Do not speculate or give " +
+    "opinions. When providing examples, keep them short and on relative.",
 };
 export async function generateContent(content: string) {
   return await gemini.models.generateContentStream({
-    model: geminiConfig.model,
-    contents: content,
+    model:
+      Bun.env.NODE_ENV === "development"
+        ? geminiConfig.model
+        : geminiConfig.fallback,
+    contents: `${geminiConfig.prompt}\n${content}`,
+    config: {
+      systemInstruction: geminiConfig.systemPrompt,
+    },
   });
 }
