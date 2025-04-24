75c5069aadaf02c2be009ff6a6bea5acf0c28e50
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-24 05:27:44 -0400

    feat(client): integrate App component into router configuration

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -1,18 +1,14 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>Hello World</div>,
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
 ]);
 
-const root = document.getElementById("root")!!;
+const root = document.getElementById("root")!;
 
-ReactDOM.createRoot(root).render(
-    <RouterProvider router={router} />
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
