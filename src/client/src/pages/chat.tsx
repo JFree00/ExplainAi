98e912f6478f4febf0c5c535b5b730b57c5d8565
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-26 07:05:00 -0400

    feat(chat): add gradient mask effect to chat scroll area

--- src/client/src/pages/chat.tsx
+++ src/client/src/pages/chat.tsx
@@ -5,7 +5,11 @@ import { Chatbar } from "../components/chat/chatbar.tsx";
 export function Chat() {
   return (
     <div className={"h-full flex flex-col"}>
-      <div className={"grow overflow-y-scroll p-3"}>
+      <div
+        className={
+          "grow overflow-y-scroll p-3 mask-b-from-black mask-b-from-90% mask-b-to-transparent"
+        }
+      >
         <div className={"w-6/12 mx-auto"}>
           <ChatWindow messages={mockChat.messages} />
         </div>
