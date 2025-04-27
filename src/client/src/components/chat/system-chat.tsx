3bd43059cadd0fed91b941f22a9589c1daefdd83
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-26 20:01:57 -0400

    fix(server): rollback irrelevant commit

--- src/client/src/components/chat/system-chat.tsx
+++ src/client/src/components/chat/system-chat.tsx
@@ -1,45 +1,14 @@
 import Markdown from "react-markdown";
 import { ChatMessage } from "../../types/chat-types";
-import { useEffect, useState } from "react";
 
 interface SystemChatProps {
   content: ChatMessage["content"];
 }
 
 export function SystemChat({ content }: SystemChatProps) {
-  const [displayedContent, setDisplayedContent] = useState<string>("");
-  useEffect(() => {
-    if (typeof content === "string") {
-      setDisplayedContent(content);
-    } else if (content instanceof ReadableStream) {
-      const reader = content.getReader();
-      const decoder = new TextDecoder();
-      let reading = true;
-      const readStream = async () => {
-        try {
-          while (reading) {
-            const { done, value } = await reader.read();
-            if (done) {
-              reading = false;
-              break;
-            } else {
-              const chunk = decoder.decode(value, { stream: true });
-              setDisplayedContent((prev) => prev + chunk);
-            }
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      readStream();
-      return () => {
-        reading = false;
-      };
-    }
-  }, [content]);
   return (
     <div className={"prose prose-invert text-white w-full max-w-full"}>
-      <Markdown>{displayedContent}</Markdown>
+      <Markdown>{content}</Markdown>
     </div>
   );
 }
