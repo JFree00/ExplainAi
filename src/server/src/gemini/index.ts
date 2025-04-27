d5c0b037e9cec3dfb5300a6310cc266e74f1fc34
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-26 20:08:16 -0400

    Merge pull request #6
    * fix(server): update prompts and environment variable reference
    
    * fix(server): add semicolon for ai content
    
    * fix(server): rollback irrelevant commit
    
    * chore(git): update .gitignore

--- src/server/src/gemini/index.ts
+++ src/server/src/gemini/index.ts
@@ -5,20 +5,20 @@ export const geminiConfig = {
   model: "gemini-2.5-flash-preview-04-17",
   fallback: "gemini-2.0-flash", // also used for development
   prompt:
-    "Analyze the provided content (which could be text or an image) and explain its main subject, meaning, or purpose" +
-    " in a clear and concise way.",
+    "Analyze the provided content (which could be text or an image). First explain what the content could mean, refer to, or be referencing. After that," +
+    "depending on the content, offer a summary, an answer, examples, or anything else applicable",
   systemPrompt:
     "You are a helpful AI assistant. Your purpose is to clearly and concisely explain text or images " +
-    "provided by the user. Do not speculate or give " +
-    "opinions. When providing examples, keep them short and on relative.",
+    "provided by the user. Do not give " +
+    "opinions.",
 };
 export async function generateContent(content: string) {
   return await gemini.models.generateContentStream({
     model:
-      Bun.env.NODE_ENV === "development"
+      Bun.env.env === "development"
         ? geminiConfig.model
         : geminiConfig.fallback,
-    contents: `${geminiConfig.prompt}\n${content}`,
+    contents: `${geminiConfig.prompt}:\n${content}`,
     config: {
       systemInstruction: geminiConfig.systemPrompt,
     },
