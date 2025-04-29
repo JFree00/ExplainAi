a128bea04d82ae3dbc5768a3cfadbd0b8631a58a
JFree00 <121167426+JFree00@users.noreply.github.com>
2025-04-29 07:10:48 -0400

    feat(server): add chat retrieval and message posting endpoints

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -4,6 +4,13 @@ import { streamText } from "hono/streaming";
 import { HTTPException } from "hono/http-exception";
 import { cors } from "hono/cors";
 import { auth } from "./auth";
+import { getCookie } from "hono/cookie";
+import {
+  createChatWithMessage,
+  selectChatById,
+} from "./database/queries/chat.ts";
+import { sendMessage } from "./database/queries/message.ts";
+import { Sender } from "./types/database-types.ts";
 
 const app = new Hono();
 app.use(cors());
@@ -30,6 +37,34 @@ app.post("/chat", async (c) => {
     },
   );
 });
+app.get("chat/:id", async (c) => {
+  const id = c.req.param("id");
+  const userId = getCookie(c, "user_Id")!;
+  const chat = await selectChatById(Number(id));
+  console.log(chat);
+});
+app.post("/message", async (c) => {
+  const input = await c.req.text();
+  const userId = getCookie(c, "user_Id")!;
+  const chat = await createChatWithMessage("new chat", userId, input);
+  console.log(chat);
+  const response = await generateContent(input);
+  let text: string;
+  return streamText(
+    c,
+    async (stream) => {
+      for await (const chunk of response) {
+        await stream.write(chunk.text!);
+        text = text ? text + chunk.text! : chunk.text!;
+      }
+      await sendMessage(Sender.System, chat, text);
+    },
+    async (error, stream) => {
+      await stream.write(error.message);
+    },
+  );
+});
+
 app.onError((error, c) => {
   if (error instanceof HTTPException) {
     // Get the custom response
